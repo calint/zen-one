e2efcecd928d81a50bde2909583d53573803f069
calint <calin.tenitchi@gmail.com>
2023-05-28 11:34:35 +0200

    .

--- notes/src-stats.sh
+++ notes/src-stats.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "wc zen-one:"
-cat zen-one.srcs/sources_1/new/* | grep -v '^[[:space:]]*$'| wc
-echo "wc zen-x gzipped:"
-cat zen-one.srcs/sources_1/new/* | grep -v '^[[:space:]]*$' | gzip | wc
+echo "zen-one"
+cat zen-one.srcs/sources_1/new/*.v | grep -vE '^\s*$|^\s*//'| wc
+echo "zen-one gzipped"
+cat zen-one.srcs/sources_1/new/*.v | grep -vE '^\s*$|^\s*//' | gzip | wc
 
-echo "wc zasm:"
-cat zasm | grep -v '^[[:space:]]*$'| wc
-echo "wc gzipped zasm:"
-cat zasm | grep -v '^[[:space:]]*$' | gzip | wc
+echo "zasm"
+cat zasm | grep -vE '^\s*$|^\s*//' | wc
+echo "zasm gzipped"
+cat zasm | grep -vE '^\s*$|^\s*//' | gzip | wc
